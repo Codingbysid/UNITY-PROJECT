6c7317426a7f75c5d5082014665ca65662aa1d54
Siddharth Gupta <132804078+Codingbysid@users.noreply.github.com>
2024-04-04 15:54:54 -0400

    Add files via upload

--- Survey.cs
+++ Survey.cs
@@ -11,10 +11,10 @@ public class Survey : MonoBehaviour
     [SerializeField] TMP_InputField question1;
     [SerializeField] TMP_Dropdown question2;
     [SerializeField] TMP_Dropdown question3;
-    [SerializeField] TMP_InputField question4;
-    [SerializeField] TMP_Dropdown question5;
+    [SerializeField] TMP_Dropdown question4;
+    [SerializeField] TMP_InputField question5;
     [SerializeField] TMP_InputField question6;
-    [SerializeField] TMP_Dropdown question7;
+    [SerializeField] TMP_InputField question7;
     [SerializeField] TMP_Dropdown question8;
     [SerializeField] TMP_InputField question9;
     [SerializeField] TMP_Dropdown question10;
@@ -24,17 +24,10 @@ public class Survey : MonoBehaviour
     [SerializeField] TMP_Dropdown question14;
     [SerializeField] TMP_Dropdown question15;
     [SerializeField] TMP_Dropdown question16;
-    [SerializeField] TMP_Dropdown question17;
+    [SerializeField] TMP_InputField question17;
     [SerializeField] TMP_Dropdown question18;
     [SerializeField] TMP_InputField question19;
     [SerializeField] TMP_InputField question20;
-    [SerializeField] TMP_Dropdown question21;
-    [SerializeField] TMP_InputField question22;
-    [SerializeField] TMP_Dropdown question23;
-    [SerializeField] TMP_Dropdown question24;
-    [SerializeField] TMP_Dropdown question25;
-    [SerializeField] TMP_Dropdown question26;
-    [SerializeField] TMP_InputField question27;
 
     string URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfIXi5h0tQquki4JTe5NYZxY_NIH1BNGZxEC0r2kVRLWWqxlA/formResponse";
 
@@ -71,10 +64,6 @@ public class Survey : MonoBehaviour
         form.AddField("entry.1375757309", question11.value);
         // Do you generally trust yourself?
         form.AddField("entry.1391903084", question12.value);
-        // Do you generally trust other drivers on the road?
-        //form.AddField("entry.1535768713", question13.value);
-        // Do you feel as though other drivers turst you on the road?
-        //form.AddField("entry.1706627105", question14.value);
         // Do you generally trust pedestrians in, on, or near the road when driving?
         form.AddField("entry.334831000", question13.value);
         // Do you feel as though pedestrians trust your driving when they're in, on, or near the road?
@@ -85,22 +74,15 @@ public class Survey : MonoBehaviour
         form.AddField("entry.2067433351", question16.value);
         // If you have heard of automated vehicles, please explain your perception of what an automated vehicle is.
         form.AddField("entry.1254119119", question17.text);
-        // What do you think the limits of an automated vehicles capabilities are?
-        //form.AddField("entry.1394819467", question20.text);
         // How would you rate your trust in an automated vehicle on the road?
         form.AddField("entry.23924560", question18.value);
-        // What might cause you to trust and automated vehicle more or less?
-        //form.AddField("entry.521943858", question22.text);
+        
         // Do you think that automated vehicles should value the life of its passengers or people outside the vehicle more?
         form.AddField("entry.2072948661", question19.value);
-        // Do you think automated vehicles will become available sometime in the future?
-        //form.AddField("entry.631576668", question24.value);
-        // Do you think automated vehicles will become available to you in the future?
-        //form.AddField("entry.1543551930", question25.value);
+        
         // If automated vehicles were to become available to you, would you use one?
         form.AddField("entry.1675933084", question20.value);
-        // What is something an automated vehicle could do that would convince you that it is safe to ride in?
-        //form.AddField("entry.339613259", question27.text);
+
 
         UnityWebRequest www = UnityWebRequest.Post(URL, form);
         yield return www.SendWebRequest();
